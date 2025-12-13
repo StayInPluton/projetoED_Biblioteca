31b6206576d1b6fb4fc3bda5ea9816507db4bcd5
karlos <karlosmarques@gmail.com>
2025-12-13 08:28:38 -0300

    livros

---
+++ livros.py
@@ -0,0 +1,32 @@
+class Livro:
+    def __init__(self, id, titulo, autor):
+        self.id = id
+        self.titulo = titulo
+        self.autor = autor
+        self.disponivel = True
+
+    def __str__(self):
+        status = "Disponível" if self.disponivel else "Emprestado"
+        return f"[{self.id}] {self.titulo} - {self.autor} ({status})"
+
+
+class GerenciadorLivros:
+    def __init__(self):
+        self.livros = {}
+
+    def cadastrar(self, id, titulo, autor):
+        if id in self.livros:
+            print("livro já cadastrado.")
+            return
+        self.livros[id] = Livro(id, titulo, autor)
+        print("Livro cadastrado com sucesso.")
+
+    def listar(self):
+        if not self.livros:
+            print("Nenhum livro cadastrado.")
+            return
+        for livro in self.livros.values():
+            print(livro)
+
+    def buscar(self, id):
+        return self.livros.get(id)
